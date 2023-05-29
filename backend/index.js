351d0a44e4b8cd8243e3934edd2fa6dada01813e
Nitin kumar <nimble2905@gmail.com>
2023-05-29 15:12:56 +0530

    some minor changes in backend

--- backend/index.js
+++ backend/index.js
@@ -12,5 +12,5 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+  console.log(`INotes backend running at http://localhost:${port}`);
 });
