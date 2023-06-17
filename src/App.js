3521fd1bedf65b214456206b2697c607930fff0a
Nitin kumar <nimble2905@gmail.com>
2023-06-17 16:58:03 +0530

    adding alert and fontawesome icon

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NoteState from "./Context/notes/NoteState";
+import Alert from "./Components/Alert";
 
 function App() {
   return (
     <NoteState>
       <Router>
         <Navbar />
+        <Alert message="this is amazing" />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/home" element={<Home />} />
