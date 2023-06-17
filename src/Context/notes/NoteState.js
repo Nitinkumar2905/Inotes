e81cb6500bc0799312b0460a96d7c7b0ac46ba75
Nitin kumar <nimble2905@gmail.com>
2023-06-17 16:56:48 +0530

    using contextapi for adding props in different components of app

--- src/Context/notes/NoteState.js
+++ src/Context/notes/NoteState.js
@@ -1,8 +1,17 @@
 import React, { useState } from "react";
-import NoteContext from "./NoteContext";
+import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
   const noteInitial = [
+    {
+      _id: "6465c165cc05d0962042b65d",
+      user: "6464b27de0cca457775a4324",
+      title: "Hostel BH1",
+      description: "Worst hostel",
+      tag: "Trash , irresponsible staff",
+      date: "2023-05-18T06:10:45.367Z",
+      __v: 0,
+    },
     {
       _id: "6465c165cc05d0962042b65d",
       user: "6464b27de0cca457775a4324",
