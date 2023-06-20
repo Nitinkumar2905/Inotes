b301efc070600f8b0271a0e2fca4e117018306c6
Nitin kumar <nimble2905@gmail.com>
2023-06-20 18:47:48 +0530

    adding login and signup button in navbar for adding authentication for the user data security

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
   let location = useLocation();
-  useEffect(() => {
-    console.log(location.pathname);
-  }, [location]);
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
             INotes
@@ -20,7 +17,8 @@ const Navbar = () => {
             data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent"
             aria-expanded="false"
-            aria-label="Toggle navigation">
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -31,7 +29,8 @@ const Navbar = () => {
                     location.pathname === "/home" ? "active" : ""
                   }`}
                   aria-current="page"
-                  to="/home">
+                  to="/home"
+                >
                   Home
                 </Link>
               </li>
@@ -40,21 +39,19 @@ const Navbar = () => {
                   className={`nav-link ${
                     location.pathname === "/about" ? "active" : ""
                   }`}
-                  to="/about">
+                  to="/about"
+                >
                   About
                 </Link>
               </li>
             </ul>
             <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
+              <Link className="btn btn-primary mx-2" to="/login">
+                Login
+              </Link>
+              <Link className="btn btn-primary mx-2" to="/signUp">
+                Sign Up
+              </Link>
             </form>
           </div>
         </div>
