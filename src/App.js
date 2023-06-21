6643fdf601cafb79b6693e8d1d09302427824000
Nitin kumar <nimble2905@gmail.com>
2023-06-21 11:58:40 +0530

    added alert component

--- src/App.js
+++ src/App.js
@@ -4,18 +4,33 @@ import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NoteState from "./Context/notes/NoteState";
-// import Alert from "./Components/Alert";
+import SignUp from "./Components/SignUp";
+import Login from "./Components/Login";
+import Alert from "./Components/Alert";
+import { useState } from "react";
 
 function App() {
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1000);
+  };
   return (
     <NoteState>
       <Router>
         <Navbar />
-        {/* <Alert message="this is amazing" /> */}
+        <Alert alert={alert} />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/home" element={<Home showAlert={showAlert} />} />
           <Route exact path="/about" element={<About />} />
+          <Route exact path="/login" element={<Login showAlert={showAlert} />} />
+          <Route exact path="/signUp" element={<SignUp showAlert={showAlert} />} />
         </Routes>
       </Router>
     </NoteState>
