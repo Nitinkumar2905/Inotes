ee380ea93804553111ff0f4d244a5114593df9a7
Nitin kumar <nimble2905@gmail.com>
2023-06-21 21:53:11 +0530

    added login authentication and fixed some issues

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const Login = (props) => {
+  const host = "http://localhost:5000";
   const [credentials, setCredentials] = useState({ email: "", password: "" });
   const Navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch(`${host}/api/auth/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -19,12 +20,12 @@ const Login = (props) => {
     });
 
     const json = await response.json();
-    // console.log(json);
+    console.log(credentials.email);
     if (json.success) {
       // save the auth token and redirect to the note page
-      localStorage.setItem("token", json.authtoken);
-      Navigate("/home");
+      localStorage.setItem("token", json.authToken);
       props.showAlert("Logged in Successfully", "success");
+      Navigate("/home");
     } else {
       props.showAlert("Invalid credentials", "danger");
     }
@@ -32,10 +33,12 @@ const Login = (props) => {
   const onChange = (e) => {
     setCredentials({ ...credentials, [e.target.name]: e.target.value });
   };
-
   return (
     <>
       <div className="container my-5" style={{ width: "50%" }}>
+        <h3 className="d-flex justify-content-center mb-5">
+          Login to Continue
+        </h3>
         <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <label htmlFor="email" className="form-label">
@@ -67,13 +70,18 @@ const Login = (props) => {
               onChange={onChange}
             />
           </div>
-          <button type="submit" className="btn btn-primary">
+          <button type="submit" className="fs-6 fw-semibold btn btn-primary">
             Login
           </button>
         </form>
-        <div className="container my-2 fs-5 " style={{}}>
-            <span className="">New User ? &nbsp;</span>
-            <Link className="text-decoration-none fw-semibold text-primary" to="/signUp">SignUp</Link>
+        <div className="container my-2 fs-5 d-flex justify-content-center ">
+          <span className="">New User ? &nbsp;</span>
+          <Link
+            className="text-decoration-none fw-semibold text-primary"
+            to="/signUp"
+          >
+            SignUp
+          </Link>
         </div>
       </div>
     </>
